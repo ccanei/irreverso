9c7207bbd67a5c8a53f89efd62b88abe98fea76a
ccanei <98197262+ccanei@users.noreply.github.com>
2026-02-19 21:29:33 -0300

    Fix layout component to include globals.css import

--- app/(surface)/layout.tsx
+++ app/(surface)/layout.tsx
@@ -1,11 +1,13 @@
+import '../globals.css';
+
 export default function RootLayout({
-  children
+  children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
     </html>
-  )
+  );
 }
